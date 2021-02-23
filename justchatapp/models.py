46bb05f252fd6877508f6a57d20b740af99956e9
Artur <artur.strz@wp.pl>
2021-02-23 21:21:58 +0100

    create message and room models

--- justchatapp/models.py
+++ justchatapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.TextField(blank=False)
+    created = models.DateTimeField(default=datetime.now)
+    active = models.BooleanField(default=True)
+
+
+class PublicMessage(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(blank=False)
+    timestamp = models.DateTimeField(default=datetime.now)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    @staticmethod
+    def load_messages(room):
+        return PublicMessage.objects.filter(room=room).order_by('-timestamp')
+
