6aa77f9ee9b98c769a0ae52d01c2c90d32a66a0e
Artur <artur.strz@wp.pl>
2021-03-05 23:55:57 +0100

    delete api

--- justchatapp/urls.py
+++ justchatapp/urls.py
@@ -4,7 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.rooms_view, name='rooms'),
     path('<str:room_name>/', views.chat_view, name='chat'),
-    path('api/get_room_messages/<int:pk_room>/', views.get_room_messages, name='get_room_messages'),
-    path('api/create_room_message/', views.create_room_messages, name='create_room_message'),
-    path('api/get_rooms/', views.get_rooms, name='get_rooms')
 ]
--- justchatapp/views.py
+++ justchatapp/views.py
@@ -1,35 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .serializers import SerializersPublicMessage, SerializersRoom
 from .models import PublicMessage, Room
-
-
-''' API '''
-@api_view(['GET'])
-def get_room_messages(request, pk_room):
-    messages = PublicMessage.objects.filter(room=pk_room).order_by('timestamp')
-    serializer = SerializersPublicMessage(messages, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def create_room_messages(request):
-    serializer = SerializersPublicMessage(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def get_rooms(request):
-    rooms = Room.objects.all()
-    serializer = SerializersRoom(rooms, many=True)
-    return Response(serializer.data)
-
-''' END API '''
-
 
 
 def rooms_view(request):
